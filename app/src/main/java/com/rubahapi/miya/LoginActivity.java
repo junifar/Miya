1f86f7edb8d775accdc7c8f5e522b197f7f435b9
junifar <junifar@gmail.com>
2017-03-10 16:50:40 +0700

    Initial commit

---
+++ app/src/main/java/com/rubahapi/miya/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.rubahapi.miya;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button_login)
+    public void onClickButtonLogin(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.button_signup)
+    public void onClickButtonSignup(View view){
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
+}
